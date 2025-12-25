6b0389a37f55cb35d349d2882e436693032afdeb
AMKOSHESH <AM.KOSHESH@GMAIL.COM>
2025-12-25 23:10:14 +0330

    pulse_detector.dart

--- lib/core/pulse_detector.dart
+++ lib/core/pulse_detector.dart
@@ -1,29 +1,41 @@
 class PulseDetector {
-  double threshold;
+  double highThreshold;
+  double lowThreshold;
   int debounceMs;
 
+  final List<double> _window = [];
+  final int windowSize = 5;
+
   DateTime _lastPulse = DateTime.fromMillisecondsSinceEpoch(0);
-  double _prevValue = 0;
+  bool _armed = true;
 
   PulseDetector({
-    required this.threshold,
+    required this.highThreshold,
+    required this.lowThreshold,
     required this.debounceMs,
   });
 
   bool process(double value) {
+    _window.add(value);
+    if (_window.length > windowSize) _window.removeAt(0);
+
+    final avg =
+        _window.reduce((a, b) => a + b) / _window.length;
+
     final now = DateTime.now();
 
-    final isRising = value > _prevValue;
-    final passedThreshold = value > threshold;
-    final debounceOk =
-        now.difference(_lastPulse).inMilliseconds > debounceMs;
-
-    _prevValue = value;
-
-    if (isRising && passedThreshold && debounceOk) {
+    if (_armed &&
+        avg > highThreshold &&
+        now.difference(_lastPulse).inMilliseconds > debounceMs) {
+      _armed = false;
       _lastPulse = now;
       return true;
     }
+
+    if (!_armed && avg < lowThreshold) {
+      _armed = true;
+    }
+
     return false;
   }
 }

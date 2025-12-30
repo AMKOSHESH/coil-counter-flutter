9f526c78724d3275b0217562a90115e2f1d9061e
AMKOSHESH <AM.KOSHESH@GMAIL.COM>
2025-12-30 23:38:59 +0330

    Update magnetometer_sensor.dart

--- lib/sensors/magnetometer_sensor.dart
+++ lib/sensors/magnetometer_sensor.dart
@@ -1,23 +1,14 @@
 import 'dart:async';
 import 'dart:math';
 import 'package:sensors_plus/sensors_plus.dart';
-import '../core/pulse_detector.dart';
 
 class MagnetometerSensor {
-  final PulseDetector detector;
-  final void Function() onPulse;
   final void Function(double) onValue;
-  final int intervalMs;
 
-  MagnetometerSensor({
-    required this.detector,
-    required this.onPulse,
-    required this.onValue,
-    required this.intervalMs,
-  });
+  MagnetometerSensor({required this.onValue});
 
   StreamSubscription start() {
-    // استفاده از بالاترین سرعت ممکن سنسور در لایه سخت‌افزار
+    // استفاده از بالاترین نرخ خروجی سخت‌افزار
     return magnetometerEventStream(samplingPeriod: SensorInterval.fastest)
         .listen((event) {
       final magnitude = sqrt(event.x * event.x + event.y * event.y + event.z * event.z);

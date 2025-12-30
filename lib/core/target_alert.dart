217f60ea5aa170ff103d0d20b0868a2b7902bae9
AMKOSHESH <AM.KOSHESH@GMAIL.COM>
2025-12-30 23:39:41 +0330

    Refactor audio playback methods in TargetAlert

--- lib/core/target_alert.dart
+++ lib/core/target_alert.dart
@@ -3,20 +3,19 @@ import 'package:vibration/vibration.dart';
 import 'dart:async';
 
 class TargetAlert {
-  late AudioPlayer _player;
+  final AudioPlayer _player = AudioPlayer();
   bool _playedFinal = false;
 
   TargetAlert() {
-    _player = AudioPlayer();
     _player.setReleaseMode(ReleaseMode.stop);
   }
 
-  Future<void> _play(double volume, {int repeats = 1}) async {
-    for (int i = 0; i < repeats; i++) {
-      await _player.stop(); // توقف بوق قبلی برای پخش سریع‌تر بعدی
-      await _player.setVolume(volume);
+  Future<void> _playSound(double vol, int count) async {
+    for (int i = 0; i < count; i++) {
+      await _player.stop();
+      await _player.setVolume(vol);
       await _player.play(AssetSource('beep.mp3'), mode: PlayerMode.lowLatency);
-      if (repeats > 1) await Future.delayed(const Duration(milliseconds: 150));
+      if (count > 1) await Future.delayed(const Duration(milliseconds: 100));
     }
   }
 
@@ -30,15 +29,12 @@ class TargetAlert {
 
     if (count >= target && !_playedFinal) {
       _playedFinal = true;
-      if (soundEnabled) _play(1.0, repeats: 5);
+      if (soundEnabled) _playSound(1.0, 5);
       if (vibrationEnabled) Vibration.vibrate(duration: 1000);
-      return;
-    }
-
-    if (target - count <= 5 && count < target) {
-      if (soundEnabled) _play(0.8, repeats: 2);
+    } else if (target - count <= 5 && count < target) {
+      if (soundEnabled) _playSound(0.7, 2);
     } else if (count < target) {
-      if (soundEnabled) _play(0.1, repeats: 1);
+      if (soundEnabled) _playSound(0.15, 1);
     }
   }
 

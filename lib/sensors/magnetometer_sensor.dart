e71c6370dfdce530b8ecb3a38d37492abd13e891
AMKOSHESH <AM.KOSHESH@GMAIL.COM>
2025-12-30 22:32:54 +0330

    Enhance MagnetometerSensor with intervalMs
    Add intervalMs parameter to MagnetometerSensor class

--- lib/sensors/magnetometer_sensor.dart
+++ lib/sensors/magnetometer_sensor.dart
@@ -7,20 +7,21 @@ class MagnetometerSensor {
   final PulseDetector detector;
   final void Function() onPulse;
   final void Function(double) onValue;
+  final int intervalMs;
 
   MagnetometerSensor({
     required this.detector,
     required this.onPulse,
     required this.onValue,
+    required this.intervalMs,
   });
 
   StreamSubscription start() {
-    return magnetometerEvents.listen((event) {
+    // استفاده از بالاترین سرعت ممکن سنسور در لایه سخت‌افزار
+    return magnetometerEventStream(samplingPeriod: SensorInterval.fastest)
+        .listen((event) {
       final magnitude = sqrt(event.x * event.x + event.y * event.y + event.z * event.z);
       onValue(magnitude);
-      if (detector.process(magnitude)) {
-        onPulse();
-      }
     });
   }
 }
